ebc1c3bf31e53988cabb057211deb44171d35b26
jgonzale5 <60185919+jgonzale5@users.noreply.github.com>
2022-11-21 18:03:01 -0500

    Raycast class

---
+++ Assets/scripts/MovementScript.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementScript : MonoBehaviour
+{
+    //The speed of the object with this script
+    public float speed;
+
+    //The length of the raycast used to determine if there's an obstacle in the way
+    public float skinWidth = 0.1f;
+
+    //What layers can be hit by the ray
+    public LayerMask skinLayers;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //We get the user input and store it in local variables
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        //We multiply the input by speed and time delta time to determine how much the player should move this frame
+        x = x * speed * Time.deltaTime;
+        y = y * speed * Time.deltaTime;
+
+        //If the function CheckForWall returns true, after being sent the direction and the range,
+        //dont move the player in that direction
+        //this.tranform.right will give us the direction vector coming to the right of the transform
+        if (CheckForWall(this.transform.right * x, skinWidth) == false)
+        {
+            x = 0;
+        }
+        if (CheckForWall(this.transform.up * y, skinWidth) == false)
+        {
+            y = 0;
+        }
+
+        //We set the new position of the transform this frame
+        //Local
+        this.transform.localPosition = this.transform.localPosition + new Vector3(x, y, 0);
+        //World
+        //this.transform.position = this.transform.position + new Vector3(x, y, 0);
+        //Equivalent to this as well
+        //this.transform.position = new Vector3(this.transform.position.x + x, 
+        //    this.transform.position.y + y, this.transform.position.z + 0);
+
+    }
+
+    bool CheckForWall(Vector3 direction, float range)
+    {
+        RaycastHit info;
+
+        //This will draw the ray in our editor for debugging purposes
+        Debug.DrawRay(this.transform.position, direction * range *2, Color.blue, 1);
+
+
+        //If we can cast a ray from the the position of this transform, in the direction specified,
+        //with a maximum distance of range, and hit something. Put the result of the hit into info.
+        //Ignore all layers not specified by skinLayers
+        if (Physics.Raycast(this.transform.position, direction, out info , range, skinLayers))
+        {
+            //This will tell us the name of the transform hit
+            Debug.Log(info.transform.name);
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+}
